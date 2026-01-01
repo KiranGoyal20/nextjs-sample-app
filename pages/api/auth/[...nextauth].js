38a33b9b1f5ad5246bdcd0b2f55f8522af158587
Kiran Goyal <Kiran.Goyal@PUN-LP03850.local>
2026-01-01 23:32:40 +0530

    fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,13 @@
 
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+// import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
   providers: [
-    GithubProvider({
-      clientId: '',
-      clientSecret: ''
-    })
+    // GithubProvider({
+    //   clientId: '',
+    //   clientSecret: ''
+    // })
   ]
 };
 
